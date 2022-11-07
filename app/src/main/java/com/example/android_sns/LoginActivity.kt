75857f3f36cbe9d827da701fc9102f11fcbbab1c
chlwlsguszz <chlwlsguszz@gmail.com>
2022-11-08 01:44:17 +0900

    로그인 화면 추가
    회원가입/로그인 분리 필요
    소셜 로그인 추가 필요

---
+++ app/src/main/java/com/example/android_sns/LoginActivity.kt
@@ -0,0 +1,58 @@
+package com.example.android_sns
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android_sns.databinding.ActivityLoginBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                val binding = ActivityLoginBinding.inflate(layoutInflater)
+                setContentView(binding.root)
+
+                binding.login.setOnClickListener {
+                        val userEmail = binding.username.text.toString()
+                        val password = binding.password.text.toString()
+                        doLogin(userEmail, password)
+                }
+
+                binding.signup.setOnClickListener {
+                        val userEmail = binding.username.text.toString()
+                        val password = binding.password.text.toString()
+                        doSignUp(userEmail, password)
+                }
+        }
+
+        private fun doLogin(userEmail: String, password: String) {
+                Firebase.auth.signInWithEmailAndPassword(userEmail, password)
+                        .addOnCompleteListener(this) {
+                                if (it.isSuccessful) {
+                                        startActivity(
+                                                Intent(this, MainActivity::class.java))
+                                        finish()
+                                } else {
+                                        Log.w("LoginActivity", "signInWithEmail", it.exception)
+                                        Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                                }
+                        }
+        }
+
+        private fun doSignUp(userEmail: String, password: String) {
+                Firebase.auth.createUserWithEmailAndPassword(userEmail, password)
+                        .addOnCompleteListener(this) {
+                                if (it.isSuccessful) {
+                                        startActivity(
+                                                Intent(this, MainActivity::class.java))
+                                        finish()
+                                } else {
+                                        Log.w("LoginActivity", "createUserWithEmail", it.exception)
+                                        Toast.makeText(this, "Sign Up failed.", Toast.LENGTH_SHORT).show()
+                                }
+                        }
+        }
+}
--- app/src/main/java/com/example/android_sns/MainActivity.kt
+++ app/src/main/java/com/example/android_sns/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.android_sns
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android_sns.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (Firebase.auth.currentUser == null) {
+            startActivity(
+                Intent(this, LoginActivity::class.java))
+            finish()
+        }
+
+        binding.textUID.text = Firebase.auth.currentUser?.uid ?: "No User"
+
+        binding.buttonSignout.setOnClickListener {
+            Firebase.auth.signOut()
+            finish()
+        }
     }
 }
