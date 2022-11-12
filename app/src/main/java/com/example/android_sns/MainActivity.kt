8418505df456ed73a2b2cbdf02d460cddf540da4
chlwlsguszz <chlwlsguszz@gmail.com>
2022-11-13 00:49:39 +0900

    오류 수정
    바텀네비게이션 사용법 변경
    
    프래그먼트 재설정

--- app/src/main/java/com/example/android_sns/MainActivity.kt
+++ app/src/main/java/com/example/android_sns/MainActivity.kt
@@ -3,13 +3,14 @@ package com.example.android_sns
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import com.example.android_sns.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private var bottomNavigationView: BottomNavigationView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +18,25 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val nhf = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
 
-        binding.bottomNavigation.setupWithNavController(nhf.navController)
+        bottomNavigationView = binding.bottomNavigation
+
+        supportFragmentManager.beginTransaction().add(R.id.fragment, DetailViewFragment())
+            .commit()
+
+        bottomNavigationView?.setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.detailViewFragment -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment, DetailViewFragment()).commit()
+                R.id.searchFragment -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment, SearchFragment()).commit()
+                R.id.messageFragment -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment, MessageFragment()).commit()
+                R.id.profileFragment -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment, ProfileFragment()).commit()
+            }
+            true
+        } )
 
         if (Firebase.auth.currentUser == null) {
             startActivity(
