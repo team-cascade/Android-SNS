2d9bd07efdb0400878b96f4d4f209e54d7e2734d
chlwlsguszz <chlwlsguszz@gmail.com>
2022-11-13 14:40:51 +0900

    회원가입 연동 + UI 수정
    SignUp Activity
    회원가입 연동
    
    Login UI 약간 수정
    
    구글 로그인 방법 수정 예정

--- app/src/main/java/com/example/android_sns/LoginActivity.kt
+++ app/src/main/java/com/example/android_sns/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android_sns.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -12,30 +13,28 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+        var auth : FirebaseAuth? = null
+
         override fun onCreate(savedInstanceState: Bundle?) {
                 super.onCreate(savedInstanceState)
                 val binding = ActivityLoginBinding.inflate(layoutInflater)
                 setContentView(binding.root)
+                auth = FirebaseAuth.getInstance()
 
-                binding.login.setOnClickListener {
-                        val userEmail = binding.username.text.toString()
-                        val password = binding.password.text.toString()
+
+                binding.btnLogin.setOnClickListener {
+                        val userEmail = binding.textID.text.toString()
+                        val password = binding.textPWD.text.toString()
                         doLogin(userEmail, password)
                 }
 
-//                binding.signup.setOnClickListener {
-//                        val userEmail = binding.username.text.toString()
-//                        val password = binding.password.text.toString()
-//                        doSignUp(userEmail, password)
-//                }
-                binding.signup.setOnClickListener {
+
+                binding.btnSignup.setOnClickListener {
                         startActivity(
                                 Intent(this, SignupActivity::class.java))
                         finish()
                 }
         }
-
-
 
         private fun doLogin(userEmail: String, password: String) {
                 Firebase.auth.signInWithEmailAndPassword(userEmail, password)
@@ -47,20 +46,6 @@ class LoginActivity : AppCompatActivity() {
                                 } else {
                                         Log.w("LoginActivity", "signInWithEmail", it.exception)
                                         Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
-                                }
-                        }
-        }
-
-        private fun doSignUp(userEmail: String, password: String) {
-                Firebase.auth.createUserWithEmailAndPassword(userEmail, password)
-                        .addOnCompleteListener(this) {
-                                if (it.isSuccessful) {
-                                        startActivity(
-                                                Intent(this, MainActivity::class.java))
-                                        finish()
-                                } else {
-                                        Log.w("LoginActivity", "createUserWithEmail", it.exception)
-                                        Toast.makeText(this, "Sign Up failed.", Toast.LENGTH_SHORT).show()
                                 }
                         }
         }
--- app/src/main/java/com/example/android_sns/SignupActivity.kt
+++ app/src/main/java/com/example/android_sns/SignupActivity.kt
@@ -2,8 +2,12 @@ package com.example.android_sns
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android_sns.databinding.ActivitySignupBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignupActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +23,23 @@ class SignupActivity: AppCompatActivity() {
         }
 
         binding.signupButton.setOnClickListener {
-            // 입력 후 처리할 코드
+            val userEmail = binding.EmailInput.text.toString()
+            val password = binding.PasswordInput.text.toString()
+            doSignUp(userEmail, password)
         }
+    }
+
+    private fun doSignUp(userEmail: String, password: String) {
+        Firebase.auth.createUserWithEmailAndPassword(userEmail, password)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    startActivity(
+                        Intent(this, MainActivity::class.java))
+                    finish()
+                } else {
+                    Log.w("SignUpActivity", "createUserWithEmail", it.exception)
+                    Toast.makeText(this, "Sign Up failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
