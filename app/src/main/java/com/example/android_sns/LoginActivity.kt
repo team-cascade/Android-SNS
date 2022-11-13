9e168df52e00a0446e3747bb99b6f4aa7ed9a8aa
chlwlsguszz <chlwlsguszz@gmail.com>
2022-11-13 15:36:54 +0900

    구글 로그인 추가
    의존성 변경
    com.google.gms:google-services:4.3.13
    -> 4.3.8

--- app/src/main/java/com/example/android_sns/LoginActivity.kt
+++ app/src/main/java/com/example/android_sns/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.android_sns
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -7,19 +8,32 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android_sns.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
-        var auth : FirebaseAuth? = null
+        private lateinit var auth : FirebaseAuth
+        private lateinit var googleSignInClient : GoogleSignInClient
 
         override fun onCreate(savedInstanceState: Bundle?) {
                 super.onCreate(savedInstanceState)
                 val binding = ActivityLoginBinding.inflate(layoutInflater)
                 setContentView(binding.root)
                 auth = FirebaseAuth.getInstance()
+
+                val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail()
+                        .build()
+
+                googleSignInClient = GoogleSignIn.getClient(this , gso)
 
 
                 binding.btnLogin.setOnClickListener {
@@ -33,6 +47,50 @@ class LoginActivity : AppCompatActivity() {
                         startActivity(
                                 Intent(this, SignupActivity::class.java))
                         finish()
+                }
+
+                binding.btnGoogleLogin.setOnClickListener {
+                        doGoogleLogin()
+                }
+        }
+
+        private fun doGoogleLogin() {
+                val signInIntent = googleSignInClient.signInIntent
+                launcher.launch(signInIntent)
+        }
+
+        private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+                        result ->
+                if (result.resultCode == Activity.RESULT_OK){
+
+                        val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                        handleResults(task)
+                }
+        }
+
+        private fun handleResults(task: Task<GoogleSignInAccount>) {
+                if (task.isSuccessful){
+                        val account : GoogleSignInAccount? = task.result
+                        if (account != null){
+                                updateUI(account)
+                        }
+                }else{
+                        Toast.makeText(this, task.exception.toString() , Toast.LENGTH_SHORT).show()
+                }
+        }
+
+        private fun updateUI(account: GoogleSignInAccount) {
+                val credential = GoogleAuthProvider.getCredential(account.idToken , null)
+                auth.signInWithCredential(credential).addOnCompleteListener {
+                        if (it.isSuccessful){
+                                val intent : Intent = Intent(this , MainActivity::class.java)
+                                //intent.putExtra("email" , account.email)
+                                //intent.putExtra("name" , account.displayName)
+                                startActivity(intent)
+                        }else{
+                                Toast.makeText(this, it.exception.toString() , Toast.LENGTH_SHORT).show()
+
+                        }
                 }
         }
 
