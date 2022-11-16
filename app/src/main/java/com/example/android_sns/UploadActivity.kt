abcc198c72c2e253722ce9de51f4aa165c5e2da0
DONG JUN KIM <90829718+dj991108@users.noreply.github.com>
2022-11-16 18:07:55 +0900

    앨범 사진 고르기

--- app/src/main/java/com/example/android_sns/UploadActivity.kt
+++ app/src/main/java/com/example/android_sns/UploadActivity.kt
@@ -2,20 +2,11 @@ package com.example.android_sns
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.BitmapFactory
-import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.example.android_sns.databinding.FragmentUploadBinding
 import com.google.firebase.storage.FirebaseStorage
 
 class UploadActivity : AppCompatActivity() {
@@ -25,7 +16,6 @@ class UploadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_upload)
-        val binding = FragmentUploadBinding.inflate(layoutInflater)
         //Initiate storage
         storage = FirebaseStorage.getInstance()
 
@@ -34,13 +24,6 @@ class UploadActivity : AppCompatActivity() {
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
                 if (it.resultCode == Activity.RESULT_OK) {
                     photoUri = it.data?.data
-                  //  Log.w("mydebug" , photoUri.toString())
-                  //  val source = ImageDecoder.createSource(this.contentResolver, photoUri)
-//                    try {
-//                        val bitmap = ImageDecoder.decodeBitmap(source)
-//                        binding.uploadImage.setImageBitmap(bitmap)
-//                    }
-//                    catch(e: Exception) {e.printStackTrace()}
                     findViewById<ImageView>(R.id.upload_image).setImageURI(photoUri)
                 } else {
                     finish()
