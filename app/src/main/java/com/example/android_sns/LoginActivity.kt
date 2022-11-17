d4b8fbc74db7b1e33e6ca98a12a4d5f9bcddf9c3
chlwlsguszz <chlwlsguszz@gmail.com>
2022-11-17 14:59:31 +0900

    회원가입 / 로그인 수정
    회원가입 메소드 추가
    
    구글 로그인 삭제
    
    파이어베이스 연동
    
    로그인, 회원가입 예외처리

--- app/src/main/java/com/example/android_sns/LoginActivity.kt
+++ app/src/main/java/com/example/android_sns/LoginActivity.kt
@@ -1,40 +1,24 @@
 package com.example.android_sns
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android_sns.databinding.ActivityLoginBinding
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
         private lateinit var auth : FirebaseAuth
-        private lateinit var googleSignInClient : GoogleSignInClient
+        private lateinit var binding: ActivityLoginBinding
 
         override fun onCreate(savedInstanceState: Bundle?) {
                 super.onCreate(savedInstanceState)
-                val binding = ActivityLoginBinding.inflate(layoutInflater)
+                binding = ActivityLoginBinding.inflate(layoutInflater)
                 setContentView(binding.root)
                 auth = FirebaseAuth.getInstance()
-
-                val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                        .requestIdToken(getString(R.string.default_web_client_id))
-                        .requestEmail()
-                        .build()
-
-                googleSignInClient = GoogleSignIn.getClient(this , gso)
-
 
                 binding.btnLogin.setOnClickListener {
                         val userEmail = binding.textID.text.toString()
@@ -49,52 +33,13 @@ class LoginActivity : AppCompatActivity() {
                         finish()
                 }
 
-                binding.btnGoogleLogin.setOnClickListener {
-                        doGoogleLogin()
-                }
-        }
-
-        private fun doGoogleLogin() {
-                val signInIntent = googleSignInClient.signInIntent
-                launcher.launch(signInIntent)
-        }
-
-        private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-                        result ->
-                if (result.resultCode == Activity.RESULT_OK){
-
-                        val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-                        handleResults(task)
-                }
-        }
-
-        private fun handleResults(task: Task<GoogleSignInAccount>) {
-                if (task.isSuccessful){
-                        val account : GoogleSignInAccount? = task.result
-                        if (account != null){
-                                updateUI(account)
-                        }
-                }else{
-                        Toast.makeText(this, task.exception.toString() , Toast.LENGTH_SHORT).show()
-                }
-        }
-
-        private fun updateUI(account: GoogleSignInAccount) {
-                val credential = GoogleAuthProvider.getCredential(account.idToken , null)
-                auth.signInWithCredential(credential).addOnCompleteListener {
-                        if (it.isSuccessful){
-                                val intent : Intent = Intent(this , MainActivity::class.java)
-                                //intent.putExtra("email" , account.email)
-                                //intent.putExtra("name" , account.displayName)
-                                startActivity(intent)
-                        }else{
-                                Toast.makeText(this, it.exception.toString() , Toast.LENGTH_SHORT).show()
-
-                        }
-                }
         }
 
         private fun doLogin(userEmail: String, password: String) {
+                if(userEmail.isNullOrEmpty() || password.isNullOrEmpty()) {
+                        binding.loginErrorText.text = "이메일과 비밀번호를 입력해주세요."
+                        return
+                }
                 Firebase.auth.signInWithEmailAndPassword(userEmail, password)
                         .addOnCompleteListener(this) {
                                 if (it.isSuccessful) {
@@ -103,7 +48,17 @@ class LoginActivity : AppCompatActivity() {
                                         finish()
                                 } else {
                                         Log.w("LoginActivity", "signInWithEmail", it.exception)
-                                        Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                                        //Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                                        var errorMessage = it.exception?.localizedMessage
+                                        when (errorMessage) {
+                                                "The email address is badly formatted." ->
+                                                                errorMessage = "이메일이 형식에 맞지 않습니다."
+                                                "There is no user record corresponding to this identifier. The user may have been deleted." ->
+                                                                errorMessage = "존재하지 않는 유저 이메일입니다."
+                                                "The password is invalid or the user does not have a password." ->
+                                                                errorMessage = "비밀번호가 일치하지 않습니다."
+                                        }
+                                        binding.loginErrorText.text = errorMessage
                                 }
                         }
         }
--- app/src/main/java/com/example/android_sns/SignupActivity.kt
+++ app/src/main/java/com/example/android_sns/SignupActivity.kt
@@ -1,18 +1,27 @@
 package com.example.android_sns
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.DatePicker
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android_sns.databinding.ActivitySignupBinding
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import java.util.*
 
 class SignupActivity: AppCompatActivity() {
+    private val db: FirebaseFirestore = Firebase.firestore
+    private val usersCollectionRef = db.collection("users")
+    private lateinit var binding: ActivitySignupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivitySignupBinding.inflate(layoutInflater)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.BackButton.setOnClickListener {
@@ -25,21 +34,81 @@ class SignupActivity: AppCompatActivity() {
         binding.signupButton.setOnClickListener {
             val userEmail = binding.EmailInput.text.toString()
             val password = binding.PasswordInput.text.toString()
-            doSignUp(userEmail, password)
+            val name = binding.NameInput.text.toString()
+            val birth = binding.BirthInput.text.toString()
+            doSignUp(userEmail, password, name, birth)
+        }
+
+        binding.BirthInput.setOnClickListener {
+            var calendar = Calendar.getInstance()
+            var year = calendar.get(Calendar.YEAR)
+            var month = calendar.get(Calendar.MONTH)
+            var day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            var dateListener = DatePickerDialog.OnDateSetListener {
+                    view, year, month, dayOfMonth ->
+                binding.BirthInput.setText("${year}${String.format("%02d",month + 1)}${String.format("%02d",dayOfMonth)}")
+            }
+
+            var builder = DatePickerDialog(this, dateListener, year, month, day)
+            builder.show()
         }
     }
 
-    private fun doSignUp(userEmail: String, password: String) {
+    private fun doSignUp(userEmail: String, password: String, name: String, birth: String) {
+        if(userEmail.isNullOrEmpty() || password.isNullOrEmpty() || name.isNullOrEmpty() || birth.isNullOrEmpty()) {
+            binding.signupErrorText.text = "입력하지 않은 항목이 있습니다."
+            return
+        }
+
+        // 파이어스토어 user 이름 중복 검사
+        usersCollectionRef.get().addOnSuccessListener { snapshot ->
+            var overlapFlag = false
+            for(doc in snapshot) {
+                if(doc.get("name") == name) {
+                    overlapFlag = true
+                }
+            }
+            if(!overlapFlag)
+                firebaseSignUp(userEmail, password, name, birth)
+            else
+                binding.signupErrorText.text = "이미 있는 사용자 이름입니다."
+        }
+    }
+
+    private fun firebaseSignUp(userEmail: String, password: String, name: String, birth: String) {
         Firebase.auth.createUserWithEmailAndPassword(userEmail, password)
             .addOnCompleteListener(this) {
                 if (it.isSuccessful) {
+                    var uid = it.result.user!!.uid
+                    addUser(uid, userEmail, name, birth.toInt())
                     startActivity(
                         Intent(this, MainActivity::class.java))
                     finish()
                 } else {
-                    Log.w("SignUpActivity", "createUserWithEmail", it.exception)
-                    Toast.makeText(this, "Sign Up failed.", Toast.LENGTH_SHORT).show()
+                    Log.w("SignUpActivity", "createUserWithEmail", it.exception )
+                    //Toast.makeText(this, "Sign Up failed.", Toast.LENGTH_SHORT).show()
+                    var errorMessage = it.exception?.localizedMessage
+                    when (errorMessage) {
+                        "The email address is badly formatted." ->
+                            errorMessage = "이메일이 형식에 맞지 않습니다."
+                        "The email address is already in use by another account." ->
+                            errorMessage = "이미 등록되어 있는 이메일입니다."
+                        "The given password is invalid. [ Password should be at least 6 characters ]" ->
+                            errorMessage = "비밀번호는 최소 6자리를 입력해야 합니다."
+                    }
+                    binding.signupErrorText.text = errorMessage
                 }
             }
+    }
+
+    private fun addUser(uid: String, userEmail: String, name: String, birth: Int) {
+        val itemMap = hashMapOf(
+            "email" to userEmail,
+            "name" to name,
+            "birth" to birth
+        )
+        usersCollectionRef.document(uid).set(itemMap)
+            .addOnSuccessListener { /*updateList()*/ }.addOnFailureListener {  }
     }
 }
